7b05c3dc73daa71d4429a013389498951fd625c4
JodiMcJodson <isekgood@gmail.com>
2024-03-29 13:39:16 -0500

    switched 2mb pages, now 1gb of memory available whooh. Also made it read the kernel from disk correctly

--- build.zig
+++ build.zig
@@ -1,23 +1,39 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.resolveTargetQuery(.{
+    const kernel_target = b.resolveTargetQuery(.{
         .cpu_arch = .x86_64,
         .os_tag = .freestanding,
+        .abi = .none,
+        .cpu_features_add = blk: {
+            var features = std.Target.Cpu.Feature.Set.empty;
+            features.addFeature(@intFromEnum(std.Target.x86.Feature.soft_float));
+            break :blk features;
+        },
+        .cpu_features_sub = blk: {
+            const Feature = std.Target.x86.Feature;
+            var features = std.Target.Cpu.Feature.Set.empty;
+            features.addFeature(@intFromEnum(Feature.mmx));
+            features.addFeature(@intFromEnum(Feature.sse));
+            features.addFeature(@intFromEnum(Feature.sse2));
+            features.addFeature(@intFromEnum(Feature.avx));
+            features.addFeature(@intFromEnum(Feature.avx2));
+            break :blk features;
+        },
     });
-    const optimize = b.standardOptimizeOption(.{
+    const kernel_optimize = b.standardOptimizeOption(.{
         .preferred_optimize_mode = .ReleaseSmall,
     });
     const kernel = b.addExecutable(.{
         .name = "kernel",
-        .target = target,
-        .optimize = optimize,
+        .target = kernel_target,
+        .optimize = kernel_optimize,
         .root_source_file = .{ .path = "kernel.zig" },
         .strip = true,
         .single_threaded = true, 
     });
     kernel.entry = .{ .symbol_name = "_start" };
-    b.installArtifact(kernel);
+    const install_kernel = b.addInstallArtifact(kernel, .{});
 
 
     const build_boot_sector = b.addSystemCommand(&.{
@@ -27,7 +43,8 @@ pub fn build(b: *std.Build) void {
         .path = "boot_sector.S",
     });
     const boot_sector = build_boot_sector.addOutputFileArg("boot_sector.bin");
-    build_boot_sector.step.dependOn(&kernel.step);
+    // depend on the install step since we need the kernel elf thingy
+    build_boot_sector.step.dependOn(&install_kernel.step);
     build_boot_sector.extra_file_dependencies = &.{
         "load_elf.inc"
     };
@@ -40,6 +57,7 @@ pub fn build(b: *std.Build) void {
     });
     run_qemu_step.addFileArg(boot_sector);
     run_qemu_step.addArgs(&.{
+        // get good debug info for interrupts
         "-d",
         "int",
         "--no-reboot",
@@ -47,7 +65,6 @@ pub fn build(b: *std.Build) void {
         "-M",
         "smm=off",
     });
-
 
     if (b.option(bool, "debugcon", "") orelse true) {
         run_qemu_step.addArgs(&.{
@@ -62,9 +79,8 @@ pub fn build(b: *std.Build) void {
         });
     }
 
-
-
     const run_step = b.step("run", "");
+    run_step.dependOn(b.getInstallStep());
     run_step.dependOn(&build_boot_sector.step);
     run_step.dependOn(&run_qemu_step.step);
 }
--- kernel.zig
+++ kernel.zig
@@ -1,12 +1,71 @@
-export fn _start() void {
-    outb(0xe9, 'A');
+const std = @import("std");
+
+const FB_ENTRY = packed struct (u16) {
+    character: u8,
+    background: u4,
+    foreground: u4,
+};
+const FRAMEBUFFER: [*]FB_ENTRY = @ptrFromInt(0xB8000);
+
+export fn _start() noreturn {
+    std.log.info("Hello, World!", .{});
+    const FRAMEBUFFER_LEN = 80*25;
+    for (FRAMEBUFFER[0..FRAMEBUFFER_LEN]) |*c| {
+        c.* = @bitCast(@as(u16, 0xFFFF));
+    }
+
+    for ("Hello, World!", 0..) |c, i| {
+        FRAMEBUFFER[i] = .{
+            .foreground = 0xF,
+            .background = 0x0,
+            .character = c,
+        };
+    }
+
     while (true) {}
 }
 
-fn outb(comptime port: u16, val: u8) void {
+inline fn outb(comptime port: u16, val: u8) void {
     asm volatile ("outb %[val], %[port]\n\t"
         :
         : [val] "{al}" (val),
-          [port] "i" (port),
+          [port] "N" (port),
     );
 }
+pub const std_options = std.Options{
+    .log_level = .debug,
+    .logFn = myLogFn,
+};
+
+
+const debugPortWriter = std.io.GenericWriter(
+    void,
+    error{},
+    debugPortWriteFn,
+){.context = {}};
+
+fn debugPortWriteFn(_: void, buffer: []const u8) error{}!usize {
+    var count: usize = 0;
+    for (buffer) |c| {
+        outb(0xe9, c);
+        count += 1;
+    }
+    return count;
+}
+
+pub fn myLogFn(
+    comptime level: std.log.Level,
+    comptime scope: @TypeOf(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype
+) void {
+    _ = scope;
+    const level_str = switch (level) {
+        .err => "err",
+        .warn => "warn",
+        .info => "info",
+        .debug => "debug",
+    };
+    try debugPortWriter.print("[{s}] " ++ format ++ "\n", .{level_str} ++ args);
+}
+
