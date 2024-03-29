e80ae7e2df787f25df93d4a12463c524545a14c5
JodiMcJodson <isekgood@gmail.com>
2024-03-29 15:46:19 -0500

    fixed build issues, should be more consistent now

--- build.zig
+++ build.zig
@@ -33,23 +33,22 @@ pub fn build(b: *std.Build) void {
         .single_threaded = true, 
     });
     kernel.entry = .{ .symbol_name = "_start" };
-    const install_kernel = b.addInstallArtifact(kernel, .{});
-
 
     const build_boot_sector = b.addSystemCommand(&.{
-        "fasm",
+        "env",
+    });
+    build_boot_sector.addPrefixedFileArg("KERNEL_ELF_FILE_PATH=", kernel.getEmittedBin());
+    build_boot_sector.addArgs(&.{
+        "fasm"
     });
     build_boot_sector.addFileArg(.{
         .path = "boot_sector.S",
     });
     const boot_sector = build_boot_sector.addOutputFileArg("boot_sector.bin");
-    // depend on the install step since we need the kernel elf thingy
-    build_boot_sector.step.dependOn(&install_kernel.step);
     build_boot_sector.extra_file_dependencies = &.{
         "load_elf.inc"
     };
-
-    b.getInstallStep().dependOn(&b.addInstallBinFile(boot_sector, "boot_sector.bin").step);
+    const install_boot_sector = b.addInstallBinFile(boot_sector, "boot_sector.bin");
 
     const run_qemu_step = b.addSystemCommand(&.{
         "qemu-system-x86_64",
@@ -80,7 +79,6 @@ pub fn build(b: *std.Build) void {
     }
 
     const run_step = b.step("run", "");
-    run_step.dependOn(b.getInstallStep());
     run_step.dependOn(&build_boot_sector.step);
     run_step.dependOn(&run_qemu_step.step);
 }
